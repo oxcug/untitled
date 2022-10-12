4ad8dc72599719d468b321f6d0c95ab40e20fb9c
Mike Choi <mkchoi212@icloud.com>
2022-10-12 10:47:10 +0100

    Initial Commit

---
+++ Crate/CrateApp.swift
@@ -0,0 +1,20 @@
+//
+//  CrateApp.swift
+//  Crate
+//
+//  Created by Mike Choi on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CrateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
